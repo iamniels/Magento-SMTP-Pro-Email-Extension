976d09c11af40c6664cf1276169c0a2e6fb7e521
nhp <nils.preuss@havoc-the-chaos.de>
2016-01-16 19:06:03 +0100

    removed some logging becuase it spammed system.log pretty heavy

--- Model/Email/Template.php
+++ Model/Email/Template.php
@@ -2,32 +2,32 @@
 
 /**
  * This class wraps the Template email sending functionality
- * If SMTP Pro is enabled it will send emails using the given 
+ * If SMTP Pro is enabled it will send emails using the given
  * configuration.
  *
  * @author Ashley Schroder (aschroder.com)
  */
- 
+
 class Aschroder_SMTPPro_Model_Email_Template extends Mage_Core_Model_Email_Template {
-	
+
     public function send($email, $name=null, array $variables = array()) {
-    	
+
     	// If it's not enabled, just return the parent result.
     	if (!Mage::helper('smtppro')->isEnabled()) {
         	 return parent::send($email, $name, $variables);
-		} 
+		}
 
-		Mage::log('SMTPPro is enabled, sending email in Aschroder_SMTPPro_Model_Email_Template');    	
-   	
-    	
+		//Mage::log('SMTPPro is enabled, sending email in Aschroder_SMTPPro_Model_Email_Template');
+
+
     	// The remainder of this function closely mirrors the parent
-    	// method except for providing the SMTP auth details from the 
-    	// configuration. This is not good OO, but the parent class 
-    	// leaves little room for useful subclassing. This will probably 
-    	// become redundant sooner or later anyway. 		
-    	
+    	// method except for providing the SMTP auth details from the
+    	// configuration. This is not good OO, but the parent class
+    	// leaves little room for useful subclassing. This will probably
+    	// become redundant sooner or later anyway.
+
     	if(!$this->isValidForSend()) {
-			Mage::log('SMTPPro: Email not valid for sending - check template, and smtp enabled/disabled setting');    	
+			Mage::log('SMTPPro: Email not valid for sending - check template, and smtp enabled/disabled setting');
     		Mage::logException(new Exception('This letter cannot be sent.')); // translation is intentionally omitted
             return false;
         }
@@ -43,30 +43,30 @@ class Aschroder_SMTPPro_Model_Email_Template extends Mage_Core_Model_Email_Templ
 
         $variables['email'] = reset($emails);
         $variables['name'] = reset($names);
-        
+
         $mail = $this->getMail();
-        
+
        	$dev = Mage::helper('smtppro')->getDevelopmentMode();
-       	
+
         if ($dev == "contact") {
-        	
+
 			$email = Mage::getStoreConfig('contacts/email/recipient_email', $this->getDesignConfig()->getStore());
-			Mage::log("Development mode set to send all emails to contact form recipient: " . $email);
-			
+			//Mage::log("Development mode set to send all emails to contact form recipient: " . $email);
+
         } elseif ($dev == "supress") {
-        	
-			Mage::log("Development mode set to supress all emails.");
+
+			//Mage::log("Development mode set to supress all emails.");
 			# we bail out, but report success
         	return true;
         }
-        
+
         // In Magento core they set the Return-Path here, for the sendmail command.
         // we assume our outbound SMTP server (or Gmail) will set that.
-        
+
         foreach ($emails as $key => $email) {
             $mail->addTo($email, '=?utf-8?B?' . base64_encode($names[$key]) . '?=');
         }
-        
+
 
         $this->setUseAbsoluteLinks(true);
         $text = $this->getProcessedTemplate($variables, true);
@@ -82,7 +82,7 @@ class Aschroder_SMTPPro_Model_Email_Template extends Mage_Core_Model_Email_Templ
 
 		// If we are using store emails as reply-to's set the header
 		// Check the header is not already set by the application.
-		// The contact form, for example, set's it to the sender of 
+		// The contact form, for example, set's it to the sender of
 		// the contact. Thanks i960 for pointing this out.
 
         if (Mage::helper('smtppro')->isReplyToStoreEmail()
@@ -95,12 +95,12 @@ class Aschroder_SMTPPro_Model_Email_Template extends Mage_Core_Model_Email_Templ
 			} else {
 	        	$mail->addHeader('Reply-To', $this->getSenderEmail());
 			}
-			Mage::log('ReplyToStoreEmail is enabled, just set Reply-To header: ' . $this->getSenderEmail());
-				
+	//		Mage::log('ReplyToStoreEmail is enabled, just set Reply-To header: ' . $this->getSenderEmail());
+
         }
 
 		$transport = Mage::helper('smtppro')->getTransport($this->getDesignConfig()->getStore());
-		
+
         try {
 
         	// adding new Event dispatch in case anyone wants to interrogate an email before being sent
@@ -111,22 +111,22 @@ class Aschroder_SMTPPro_Model_Email_Template extends Mage_Core_Model_Email_Templ
                     'template' => $this->getTemplateId(),
                      'subject' => $this->getProcessedTemplateSubject($variables),
         	));
-                
-        	Mage::log('About to send email');
+
+        	//Mage::log('About to send email');
 	        $mail->send($transport); // Zend_Mail warning..
-		    Mage::log('Finished sending email');
-		    
+		    //Mage::log('Finished sending email');
+
 		    // Record one email for each receipient
          	foreach ($emails as $key => $email) {
-				Mage::dispatchEvent('smtppro_email_after_send', 
+				Mage::dispatchEvent('smtppro_email_after_send',
 					 array('to' => $email,
 						 'template' => $this->getTemplateId(),
 						 'subject' => $this->getProcessedTemplateSubject($variables),
 						 'html' => !$this->isPlain(),
 						 'email_body' => $text));
-				 	
+
         	}
-        	
+
 	        $this->_mail = null;
         } catch (Exception $e) {
         	Mage::logException($e);
